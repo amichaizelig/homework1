32e8c6b09e8bc5e8c4ac1d45be505f7611b08f63
amichai <amichai@zelig.co.il>
2021-12-23 23:21:30 +0200

    last commit

--- file1.py
+++ file1.py
@@ -4,7 +4,7 @@ from two import getting_input_from_user
 def main():
     a = getting_input_from_user()
     b = getting_input_from_user()
-    result = addition(a, c)
+    result = addition(a, b)
     print(f"the sum is {result} ")
 
 main()
