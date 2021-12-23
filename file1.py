04e328f6e5dcf0553a05e5a7b11260cbe41cce9e
amichai <amichai@zelig.co.il>
2021-12-23 14:31:58 +0200

    1st commit befor any changes

---
+++ file1.py
@@ -0,0 +1,10 @@
+from one import addition
+from two import getting_input_from_user
+
+def main():
+    a = getting_input_from_user()
+    b = getting_input_from_user()
+    result = addition(a, b)
+    print(result)
+
+main()
---
+++ two.py
@@ -0,0 +1,4 @@
+def getting_input_from_user():
+    input_to_return = int(input("enter number: "))
+    return  input_to_return
+
